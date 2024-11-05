debf92534696ea3c4dde4bc674b62275f72311a3
Patrick Kerschbaum <patrick.kerschbaum@gmail.com>
2024-11-05 15:17:56 +0100

    initial commit

---
+++ .prettierrc.cjs
@@ -0,0 +1,11 @@
+module.exports = {
+  singleQuote: true,
+  trailingComma: 'all',
+  printWidth: 100,
+  endOfLine: 'auto',
+  // third-party dependencies first, workspace dependencies second, internal stuff third
+  importOrder: ['<THIRD_PARTY_MODULES>', '', '^#pkg/(.*)$', '', '^[./]'],
+  importOrderParserPlugins: ['typescript', 'jsx', 'decorators-legacy'],
+  importOrderTypeScriptVersion: '5.5.4',
+  plugins: ['prettier-plugin-packagejson', '@ianvs/prettier-plugin-sort-imports'],
+};
